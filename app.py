62b4b37aaffdafb75aa05ec81e2a9666c92ffc22
Shon Rockey <shonmon168@gmail.com>
2024-03-29 23:53:50 +0530

    Added embeddings

---
+++ app.py
@@ -0,0 +1,72 @@
+from llm import llm
+from embeddings import embeddings
+# Bring in streamlit for UI/app interface
+import streamlit as st
+# Import PDF document loaders...there's other ones as well!
+from langchain_community.document_loaders import PyPDFLoader
+# Import chroma as the vector store 
+from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+
+
+# Import vector store stuff
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
+
+
+# Can sub this out for other LLM providers
+
+
+# Create instance of  LLM
+llm = llm()
+embeddings = embeddings()
+
+# Create and load PDF Loader
+loader = PyPDFLoader('annualreport.pdf')
+# Split pages from pdf 
+documents = loader.load_and_split()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+texts_chunks = text_splitter.split_documents(documents)
+store = Chroma.from_documents(texts_chunks, embeddings, collection_name='annualreport')
+vectorstore_info = VectorStoreInfo(
+    name="annual_report",
+    description="a banking annual report as a pdf",
+    vectorstore=store
+)
+# Convert the document store into a langchain toolkit
+toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info, llm=llm)
+
+# Add the toolkit to an end-to-end LC
+agent_executor = create_vectorstore_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True
+)
+st.title('🦜🔗 GPT Investment Banker')
+# Create a text input box for the user
+prompt = st.text_input('Input your prompt here')
+
+# If the user hits enter
+if prompt:
+    # Then pass the prompt to the LLM
+    response = agent_executor.run(prompt)
+    # ...and write it out to the screen
+    st.write(response)
+
+    # With a streamlit expander  
+    with st.expander('Document Similarity Search'):
+        # Find the relevant pages
+        search = store.similarity_search_with_score(prompt) 
+        # Write out the first 
+        st.write(search[0][0].page_content) 
---
+++ embeddings.py
@@ -0,0 +1,21 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+
+def embeddings():
+    # Define the path to the pre-trained model you want to use
+    modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+
+    # Create a dictionary with model configuration options, specifying to use the CPU for computations
+    model_kwargs = {'device':'cpu'}
+
+    # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
+    encode_kwargs = {'normalize_embeddings': False}
+
+    # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+    embeddings = HuggingFaceEmbeddings(
+        model_name=modelPath,     # Provide the pre-trained model's path
+        model_kwargs=model_kwargs, # Pass the model configuration options
+        encode_kwargs=encode_kwargs # Pass the encoding options
+)
+    return embeddings
+
